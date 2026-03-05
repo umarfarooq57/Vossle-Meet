89a26a848bfdb5ecda19ff6b2270aac319e127cc
Umar Farooq <umarfarooq5743@gmail.com>
2026-03-06 00:24:20 +0500

    fix(prisma): add required options object for Prisma 7 compatibility

--- server/src/config/prisma.client.js
+++ server/src/config/prisma.client.js
@@ -4,6 +4,8 @@
 
 const { PrismaClient } = require('@prisma/client');
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+    log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
+});
 
 module.exports = prisma;
